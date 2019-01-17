f64c6051acdf698cd94e9a33cc1e67ab9d63b7ac
hizqeelrz <hizqeel1214@gmail.com>
2019-01-17 07:11:43 +0500

    functionality with router added in the board

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,7 +1,52 @@
 import React from 'react';
 
-const Recipe = () => (
-    <div>Recipe Component!</div>
-);
+const API_KEY= "9e8702e11e7e8fdcc951cead9b0ae517";
+
+class Recipe extends React.Component {
+
+    state = {
+        activeRecipe: []
+    }
+
+    // life cycle hook
+    // loads as soon as this is loaded or mounted on the web browser,
+    // it trigger as soon as component loads on Screen
+    componentDidMount = async () => {
+        const title = this.props.location.state.recipe;
+
+        // incase of api cors error load this link https://cors-anywhere.herokuapp.com
+        const req = await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=${title}`);
+    
+        //whatever called from the api will be stored in the data variable
+        const res = await req.json();
+
+        this.setState({
+            activeRecipe: res.recipes[0]
+        });
+        console.log(this.state.activeRecipe);
+    }
+
+    render(){
+
+        const recipe = this.state.activeRecipe;
+
+        return (
+            <div className="container">
+                <div className="active-recipe">
+                    <img className="active-recipe__img" src={recipe.image_url} alt={recipe.title} />
+                    <h3 className="active-recipe__title">{recipe.title}</h3>
+                    <h4 className="active-recipe__publisher">Publisher:  <span>
+                        {recipe.publisher}
+                        </span>
+                    </h4>
+                    <p className="active-recipe__website">
+                        <span><a href={recipe.publisher_url} >{recipe.publisher_url}</a></span>
+                    </p>
+                    <button className="active-recipe__button" >Go Home</button>
+                </div>
+            </div>
+        );
+    }
+}
 
 export default Recipe;
